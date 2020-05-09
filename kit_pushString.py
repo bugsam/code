aecbc385191f46fa0fbe485d714aba56c841e360
bugsam <44240720+bugsam@users.noreply.github.com>
2020-05-09 18:10:58 -0300

    Update kit_pushString.py

--- kit_pushString.py
+++ kit_pushString.py
@@ -3,17 +3,15 @@
 
 __author__ = "@bugsam"
 
-code = 'TYPE YOUR STRING HERE'
-print("String length: " + hex(len(code)))
+code = 'TYPE SOME STRING HERE'
 code = code[::-1].encode('hex')
-
-#code = 'TYPE YOUR SHELLCODE HERE'
+code = 'TYPE YOUR OPCODES HERE'
+print("String lenght: " + hex(len(code)/2))
 mod = len(code)%8
 if mod != 0:
     count = (8 - mod)/2
     #adding padding
     code = ("90" * count) + code
-
 i = 0
 x = 0
 y = 8
@@ -22,5 +20,4 @@ while i != (len(code)/8):
     x = y
     y += 8
     i += 1
-    
-print("String length w/ padding: " + hex(len(code)/2))
+print("String lenght w/ padding: " + hex(len(code)/2))
