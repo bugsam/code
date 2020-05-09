bf5646a9c708c0df4777d06ac7feda7eb882a218
bugsam <44240720+bugsam@users.noreply.github.com>
2020-05-09 17:02:17 -0300

    Update kit_pushString.py

--- kit_pushString.py
+++ kit_pushString.py
@@ -1,17 +1,19 @@
-/!/usr/bin/env python2
-# 05/09/2020
-# code to assist 'push shellcode' technique
+#!/usr/bin/env python2
+#05/09/2020
 
 __author__ = "@bugsam"
 
 code = 'TYPE YOUR STRING HERE'
+print("String lenght: " + hex(len(code)))
 code = code[::-1].encode('hex')
+
 #code = 'TYPE YOUR SHELLCODE HERE'
 mod = len(code)%8
 if mod != 0:
     count = (8 - mod)/2
     #adding padding
     code = ("90" * count) + code
+
 i = 0
 x = 0
 y = 8
@@ -20,3 +22,5 @@ while i != (len(code)/8):
     x = y
     y += 8
     i += 1
+    
+print("String lenght w/ padding: " + hex(len(code)/2))
