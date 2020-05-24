859c16b79a57d31801399dca942f83e55031e2fc
bugsam <44240720+bugsam@users.noreply.github.com>
2020-05-23 21:09:54 -0300

    Update toolkit_opcode_generator.py

--- toolkit_opcode_generator.py
+++ toolkit_opcode_generator.py
@@ -1,5 +1,5 @@
 # Author: @bugsam
-# 
+# 05/23/2020
 # sys.argv[1] = string:: path to elf
 # sys.argv[2] = string:: one byte key (e.g. '0xAA')
 import sys
@@ -12,12 +12,14 @@ def shellcode(bytes):
 	x = 0
 	y = 2
 	code  = ''
+	ncode = ''
 	while i != (len(ops)/2):
 		code += "\\x" + ops[x:y]
+		ncode += '0x%02x,'% (int(ops[x:y],16))
 		x = y
 		y += 2
 		i += 1
-	return code
+	return code, ncode
 
 def cipher(bytes,key):
 	ops = bytes
@@ -43,15 +45,18 @@ f = open(path, 'rb')	#read binary
 elf = ELFFile(f)	# map file as ELF
 code = elf.get_section_by_name('.text')	# extract .text section
 ops = code.data()	# get data from text
-xcode = (shellcode(ops.hex()))
+xcode, ncode = (shellcode(ops.hex())) #returns payload with \ and ,
 
 key = sys.argv[2]
 encoded = (cipher(ops.hex(),key))
+xencoded, nencoded = shellcode(encoded)
 old = (cipher(encoded,key))
 print("Key works:",ops.hex() == old and no_nullbyte(encoded))
 print()
 print("Original:",ops.hex())
 print("Payload:",xcode)
+print("Payload:",ncode)
 print()
 print("Encoded:",encoded)
-print("Payload:",shellcode(encoded))
+print("Payload:",xencoded)
+print("Payload:",nencoded)
