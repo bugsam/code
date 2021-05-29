6c19c46050eb0e2b37cd832507b650611cb700f4
bugsam <44240720+bugsam@users.noreply.github.com>
2021-05-29 18:02:04 -0300

    Update calc_2numbers.c

--- calc_2numbers.c
+++ calc_2numbers.c
@@ -1,7 +1,3 @@
-/*@bugsam
-05/29/2021
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -9,6 +5,8 @@ struct num{
     char digit;
     struct num *next;
 };
+
+struct num *join (struct num *nbit, struct num *bit);
 
 int main(void){
     //! showMemory(msb)
@@ -28,10 +26,16 @@ int main(void){
         if(a != 0x0a){
             bit = (struct num *) malloc(sizeof(struct num));
             
-            nbit->next = bit;
-            nbit = bit;
+            nbit = join(nbit, bit);
+            //nbit->next = bit;
+            //nbit = bit;
         }
     }
-    
     printf("sum: %d",sum);
+}
+
+struct num *join(struct num *nbit, struct num *bit){
+    nbit->next = bit;
+    nbit = bit;
+    return(nbit);
 }
