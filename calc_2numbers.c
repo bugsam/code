d88db42f2d862cb5f66234482a94c447d473e99b
bugsam <44240720+bugsam@users.noreply.github.com>
2021-05-29 19:19:50 -0300

    Update calc_2numbers.c

--- calc_2numbers.c
+++ calc_2numbers.c
@@ -11,35 +11,62 @@ struct num{
     struct num *next;
 };
 
-struct num *join (struct num *nbit, struct num *bit);
+struct num *join (struct num *, struct num *);
+struct num *createNumber(char);
+struct num *generateNumber(void);
+int sum(struct num *, struc num *);
 
 int main(void){
-    struct num *bit=NULL, *nbit; //
-    char a;
-    int sum = 0;
+    //! showMemory(msb)
+    struct num *a, *b;
+    int result = 0;
     
-    scanf("%c",&a);
-    bit = (struct num *) malloc(sizeof(struct num));
-    nbit = bit;
+    a = generateNumber();
+    b = generateNumber();
+    result = sum(a,b);
     
-    while(a != 0x0a){
-        a -= 0x30;
-        bit->digit = a;
-        scanf("%c",&a);
-        
-        if(a != 0x0a){
-            bit = (struct num *) malloc(sizeof(struct num));
-            
-            nbit = join(nbit, bit);
-            //nbit->next = bit;
-            //nbit = bit;
-        }
-    }
-    printf("sum: %d",sum);
+    
+    return 0;
 }
 
 struct num *join(struct num *nbit, struct num *bit){
     nbit->next = bit;
     nbit = bit;
+
     return(nbit);
+}
+
+struct num *createNumber(char c){
+    struct num *bit;
+    bit = (struct num *) malloc(sizeof(struct num));
+    c -= 0x30;
+    bit->digit = c;
+
+    return(bit);
+}
+
+struct num *generateNumber(void){
+    struct num *start=NULL, *bit, *nbit;
+    char a;
+    int sum = 0;
+    
+    scanf("%c",&a);
+    while(a != 0x0a){
+        if(start == NULL){
+            bit = createNumber(a);
+            start = nbit = bit;
+        } else {
+            bit = createNumber(a);
+            nbit = join(nbit, bit);
+        }
+        scanf("%c",&a);
+    }
+    return(start);
+}
+
+int sum(struct num *a, struc num *b){
+    int result = 0;
+    
+    
+    
 }
