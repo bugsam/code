2147ab1d900f005986e78b74311a8f4f27c58a32
Samuel Almeida <44240720+bugsam@users.noreply.github.com>
2021-11-20 23:24:01 -0300

    Update fractal-tree-problem.sh

--- shell/fractal-tree-problem.sh
+++ shell/fractal-tree-problem.sh
@@ -5,20 +5,106 @@ COLUMNS=100
 
 read LINE
 
+if [[ $LINE -eq 1 ]]
+then
+    #line
+    for (( x=1; x<=(ROWS/2); x++))
+    do
+        for (( i=0; i<COLUMNS; i++ ))
+        do
+            printf "_"
+        done
+        printf "\n"
+    done
+fi
+
+
+if [[ $LINE -eq 2 ]]
+then
+    
+    let "BEG=(ROWS/4)+10"
+    let "MED=(ROWS/4)"
+    let "INT=(ROWS/4)"
+    let "END=BEG+1"
+    
+    #line
+    for (( x=1; x<=(ROWS/4); x++))
+    do
+        for (( i=0; i<COLUMNS; i++ ))
+        do
+            printf "_"
+        done
+        printf "\n"
+    done
+
+    #first
+    for (( x=0; x<=ROWS/8; x++))
+    do
+        for (( i=0; i<BEG; i++ ))
+        do
+            printf "_"
+        done
+        printf "1"
+        let "BEG++"
+    
+    
+        for (( i=0; i<MED; i++ ))
+        do
+            printf "_"
+        done
+        printf "1"
+    
+        for (( i=0; i<INT; i++ ))
+        do
+            printf "_"
+        done
+        printf "1"
+        let "INT++"
+        let "INT++"
+        
+        for (( i=0; i<MED; i++ ))
+        do
+            printf "_"
+        done
+        printf "1"
+        let "MED--"
+        let "MED--"
+    
+        for (( i=0; i<END; i++ ))
+        do
+            printf "_"
+        done
+        printf "\n"
+        let "END++"
+    done
+fi
+
+
 let "BEG=(ROWS/2)+2"
 let "MED=(ROWS/2)"
 let "END=BEG+1"
 
-#base2
-for (( x=1; x<=(ROWS/2); x++))
+#base
+for (( x=0; x<=ROWS/8; x++))
 do
-    for (( i=0; i<COLUMNS; i++ ))
+    for (( i=0; i<BEG; i++ ))
+    do
+        printf "_"
+    done
+    printf "1"
+    
+    for (( i=0; i<MED; i++ ))
+    do
+        printf "_"
+    done
+    printf "1"
+
+    for (( i=0; i<END; i++ ))
     do
         printf "_"
     done
     printf "\n"
 done
-
 
 #first
 for (( x=0; x<=ROWS/4; x++))
