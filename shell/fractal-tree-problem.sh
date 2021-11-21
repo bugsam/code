5d975f6d108e23a3c0080c228fa5af75f56e9fbb
Samuel Almeida <44240720+bugsam@users.noreply.github.com>
2021-11-20 23:51:36 -0300

    Update fractal-tree-problem.sh

--- shell/fractal-tree-problem.sh
+++ shell/fractal-tree-problem.sh
@@ -5,9 +5,98 @@ COLUMNS=100
 
 read LINE
 
+
+if [[ $LINE -gt 1 ]]
+then
+    
+    let "LINE1=(ROWS/3)+4"
+    let "LINE24=(ROWS/4)"
+    let "LINE3=(ROWS/4)"
+    let "LINE5=LINE1+1"
+    
+    #headerTwo
+    for (( x=1; x<=(ROWS/4); x++))
+    do
+        for (( i=0; i<COLUMNS; i++ ))
+        do
+            printf "_"
+        done
+        printf "\n"
+    done
+
+    #Two
+    for (( x=0; x<=ROWS/8; x++))
+    do
+        for (( i=0; i<LINE1; i++ ))
+        do
+            printf "_"
+        done
+        printf "1"
+        let "LINE1++"
+    
+    
+        for (( i=0; i<LINE24; i++ ))
+        do
+            printf "_"
+        done
+        printf "1"
+    
+        for (( i=0; i<LINE3; i++ ))
+        do
+            printf "_"
+        done
+        printf "1"
+        let "LINE3++"
+        let "LINE3++"
+        
+        for (( i=0; i<LINE24; i++ ))
+        do
+            printf "_"
+        done
+        printf "1"
+        let "LINE24--"
+        let "LINE24--"
+    
+        for (( i=0; i<LINE5; i++ ))
+        do
+            printf "_"
+        done
+        printf "\n"
+        let "LINE5++"
+    done
+    
+    let "LINE1=(ROWS/2)+2"
+    let "LINE2=(ROWS/2)"
+    let "LINE3=LINE1+1"
+
+    #baseTwo
+    for (( x=0; x<=ROWS/8; x++))
+    do
+        for (( i=0; i<LINE1; i++ ))
+        do
+            printf "_"
+        done
+        printf "1"
+        
+        for (( i=0; i<LINE2; i++ ))
+        do
+            printf "_"
+        done
+        printf "1"
+    
+        for (( i=0; i<LINE3; i++ ))
+        do
+            printf "_"
+        done
+        printf "\n"
+    done
+fi
+
+
+
 if [[ $LINE -eq 1 ]]
 then
-    #line
+    #headerOne
     for (( x=1; x<=(ROWS/2); x++))
     do
         for (( i=0; i<COLUMNS; i++ ))
@@ -18,122 +107,38 @@ then
     done
 fi
 
+#footer
+let "LINE1=(ROWS/2)+2"
+let "LINE2=(ROWS/2)"
+let "LINE3=LINE1+1"
 
-if [[ $LINE -eq 2 ]]
-then
-    
-    let "BEG=(ROWS/4)+10"
-    let "YAH=(ROWS/2)"
-    let "MED=(ROWS/4)"
-    let "INT=(ROWS/4)"
-    let "END=BEG+1"
-    
-    #line
-    for (( x=1; x<=(ROWS/4); x++))
-    do
-        for (( i=0; i<COLUMNS; i++ ))
-        do
-            printf "_"
-        done
-        printf "\n"
-    done
-
-    #first
-    for (( x=0; x<=ROWS/8; x++))
-    do
-        for (( i=0; i<BEG; i++ ))
-        do
-            printf "_"
-        done
-        printf "1"
-        let "BEG++"
-    
-    
-        for (( i=0; i<MED; i++ ))
-        do
-            printf "_"
-        done
-        printf "1"
-    
-        for (( i=0; i<INT; i++ ))
-        do
-            printf "_"
-        done
-        printf "1"
-        let "INT++"
-        let "INT++"
-        
-        for (( i=0; i<MED; i++ ))
-        do
-            printf "_"
-        done
-        printf "1"
-        let "MED--"
-        let "MED--"
-    
-        for (( i=0; i<END; i++ ))
-        do
-            printf "_"
-        done
-        printf "\n"
-        let "END++"
-    done
-    
-    #base
-    for (( x=0; x<=ROWS/8; x++))
-    do
-        for (( i=0; i<BEG; i++ ))
-        do
-            printf "_"
-        done
-        printf "1"
-        
-        for (( i=0; i<YAH; i++ ))
-        do
-            printf "_"
-        done
-        printf "1"
-    
-        for (( i=0; i<END; i++ ))
-        do
-            printf "_"
-        done
-        printf "\n"
-    done
-fi
-
-
-let "BEG=(ROWS/2)+2"
-let "MED=(ROWS/2)"
-let "END=BEG+1"
-
-#first
+#One
 for (( x=0; x<=ROWS/4; x++))
 do
-    for (( i=0; i<BEG; i++ ))
+    for (( i=0; i<LINE1; i++ ))
     do
         printf "_"
     done
     printf "1"
-    let "BEG++"
+    let "LINE1++"
     
-    for (( i=0; i<MED; i++ ))
+    for (( i=0; i<LINE2; i++ ))
     do
         printf "_"
     done
     printf "1"
-    let "MED--"
-    let "MED--"
+    let "LINE2--"
+    let "LINE2--"
 
-    for (( i=0; i<END; i++ ))
+    for (( i=0; i<LINE3; i++ ))
     do
         printf "_"
     done
     printf "\n"
-    let "END++"
+    let "LINE3++"
 done
 
-#base
+#baseOne
 for (( x=0; x<=ROWS/4; x++))
 do
     for (( i=0; i<(COLUMNS/2-1); i++ ))
